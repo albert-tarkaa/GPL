17905a036aaa771cb39f5a785763a1f7d127f10e
albert-tarkaa <tarkaa.albert@gmail.com>
2023-11-10 01:58:23 +0100

    feat: Reset command implementation

---
+++ Commands/ResetCommand.cs
@@ -0,0 +1,31 @@
+﻿using GPL.Commands;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GPL.Utilities
+{
+    public class ResetCommand : ICommand
+    {
+        private int defaultTargetX, defaultTargetY;
+        CordinatesStateManager stateManager;
+        Bitmap bitmap;
+        PictureBox pictureBox;
+
+
+        public ResetCommand(int x, int y, CordinatesStateManager cordinatesStateManager, Bitmap bitmap, PictureBox pictureBox)
+        {
+            this.defaultTargetX = x;
+            this.defaultTargetY = y;
+            this.stateManager = cordinatesStateManager;
+            this.bitmap = bitmap;
+            this.pictureBox = pictureBox;
+        }
+
+        public void Execute(Graphics g)
+        {
+            stateManager.SetCordinates(defaultTargetX, defaultTargetY);
+            stateManager.DrawCursor(bitmap);
+            pictureBox.Refresh();
+        }
+    }
+}
