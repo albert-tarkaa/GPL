5a7b0cc60616b29c1fc0788da6425dc435a8ea9f
albert-tarkaa <tarkaa.albert@gmail.com>
2023-11-13 06:13:48 +0100

    XML and unit tests for the FillCommand

---
+++ GPLTests/FillCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using GPL.Commands;
+using GPL.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPLTests
+{
+    [TestClass]
+    public class FillCommandTests
+    {
+        [TestMethod]
+        public void Execute_ValidCommand_SetsFillStateAndRefreshesPictureBox()
+        {
+            // Arrange
+            var coordinatesManager = new DrawingSettings();
+            var pictureBox = new PictureBox();
+            var fillCommand = new FillCommand(coordinatesManager, pictureBox);
+
+            // Act
+            fillCommand.Execute(Graphics.FromImage(new Bitmap(1, 1)));
+
+            // Assert
+            Assert.IsTrue(coordinatesManager.fill);
+        }
+
+        [TestMethod]
+        public void Execute_InvalidCommand_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            var coordinatesManager = new DrawingSettings();
+            var pictureBox = new PictureBox();
+            var fillCommand = new FillCommand(coordinatesManager, pictureBox);
+
+            // Act & Assert
+            Assert.ThrowsException<InvalidOperationException>(() => fillCommand.Execute(null));
+        }
+    }
+}
--- GPLv2/Commands/FillCommand.cs
+++ GPLv2/Commands/FillCommand.cs
@@ -2,19 +2,37 @@
 
 namespace GPL.Commands
 {
+    /// <summary>
+    /// Represents a command to set the fill state and refresh the PictureBox.
+    /// </summary>
     public class FillCommand : ICommand
     {
-        DrawingSettings stateManager;
-        PictureBox pictureBox;
-        public FillCommand(DrawingSettings cordinatesStateManager, PictureBox pictureBox)
+        private readonly DrawingSettings stateManager;
+        private readonly PictureBox pictureBox;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FillCommand"/> class.
+        /// </summary>
+        /// <param name="coordinatesManager">The drawing settings manager.</param>
+        /// <param name="pictureBox">The PictureBox used for drawing.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="coordinatesManager"/> or <paramref name="pictureBox"/> is null.</exception>
+        public FillCommand(DrawingSettings coordinatesManager, PictureBox pictureBox)
         {
-            this.stateManager = cordinatesStateManager;
-            this.pictureBox = pictureBox;
+            this.stateManager = coordinatesManager ?? throw new ArgumentNullException(nameof(coordinatesManager), "Drawing settings manager cannot be null.");
+            this.pictureBox = pictureBox ?? throw new ArgumentNullException(nameof(pictureBox), "PictureBox cannot be null.");
         }
 
+        /// <summary>
+        /// Executes the fill command by setting the fill state and refreshing the PictureBox.
+        /// </summary>
+        /// <param name="g">The graphics context for command execution.</param>
+        /// <exception cref="InvalidOperationException">Thrown if an error occurs during command execution.</exception>
         public void Execute(Graphics g)
         {
-
+            if (g == null)
+            {
+                throw new InvalidOperationException( "Graphics context cannot be null.");
+            }
             try
             {
                 stateManager.SetFill();
@@ -22,7 +40,7 @@ namespace GPL.Commands
             }
             catch (Exception ex)
             {
-                throw new InvalidOperationException($"Error executing 'Fill' command: {ex.Message}");
+                throw new InvalidOperationException($"Error executing 'Fill' command: {ex.Message}", ex);
             }
         }
     }
