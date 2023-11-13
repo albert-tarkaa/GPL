df77e495697dcce1087c049a0f239ce8e24a779d
albert-tarkaa <tarkaa.albert@gmail.com>
2023-11-13 06:13:27 +0100

    XML and unit tests for the ColourCommand

---
+++ GPLTests/ColorCommandTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using GPL.Commands;
+using GPL.Utilities;
+
+namespace GPLTests
+{
+    [TestClass]
+    public class ColorCommandTests
+    {
+        [TestMethod]
+        public void Constructor_NullCoordinatesManager_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var pictureBox = new PictureBox();
+            var color = Color.Red;
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new ColorCommand(null, pictureBox, color));
+        }
+
+        [TestMethod]
+        public void Constructor_NullPictureBox_ThrowsArgumentNullException()
+        {
+            // Arrange
+            var coordinatesManager = new DrawingSettings();
+            var color = Color.Red;
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new ColorCommand(coordinatesManager, null, color));
+        }
+
+        [TestMethod]
+        public void Execute_ValidParameters_SetsColorAndRefreshesPictureBox()
+        {
+            // Arrange
+            var coordinatesManager = new DrawingSettings();
+            var pictureBox = new PictureBox();
+            var color = Color.Red;
+            var graphics = Graphics.FromImage(new Bitmap(100, 100));
+            var colorCommand = new ColorCommand(coordinatesManager, pictureBox, color);
+
+            // Act
+            colorCommand.Execute(graphics);
+
+            // Assert
+            Assert.AreEqual(color, coordinatesManager.color);
+        }
+    }
+
+
+}
---
+++ GPLv2/Commands/ColorCommand.cs
@@ -0,0 +1,50 @@
+﻿using GPL.Utilities;
+using System;
+using System.Drawing;
+
+namespace GPL.Commands
+{
+    /// <summary>
+    /// Tests the command to set the color.
+    /// </summary>
+    public class ColorCommand : ICommand
+    {
+        private readonly DrawingSettings stateManager;
+        private readonly PictureBox pictureBox;
+        private readonly Color color;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorCommand"/> class.
+        /// </summary>
+        /// <param name="cordinatesStateManager">The drawing settings manager.</param>
+        /// <param name="picturebox">The PictureBox used for drawing.</param>
+        /// <param name="c">The color to set.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="cordinatesStateManager"/> or <paramref name="picturebox"/> is null.
+        /// </exception>
+        public ColorCommand(DrawingSettings cordinatesStateManager, PictureBox picturebox, Color c)
+        {
+            this.stateManager = cordinatesStateManager ?? throw new ArgumentNullException(nameof(cordinatesStateManager));
+            this.pictureBox = picturebox ?? throw new ArgumentNullException(nameof(picturebox));
+            this.color = c;
+        }
+
+        /// <summary>
+        /// Executes the color command by setting the color in the drawing context and refreshing the PictureBox.
+        /// </summary>
+        /// <param name="g">The graphics context for the execution.</param>
+        /// <exception cref="InvalidOperationException">Thrown if an error occurs during the execution.</exception>
+        public void Execute(Graphics g)
+        {
+            try
+            {
+                stateManager.SetColor(color);
+                pictureBox.Refresh();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error executing 'Color' command: {ex.Message}");
+            }
+        }
+    }
+}
