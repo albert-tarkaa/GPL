80469174d82742c51ddd0033d90546df55c91a83
albert-tarkaa <tarkaa.albert@gmail.com>
2024-01-13 22:55:27 +0100

    doc: XML documentation for the interface

--- GPLv2/Commands/ICommand.cs
+++ GPLv2/Commands/ICommand.cs
@@ -1,7 +1,14 @@
 ﻿namespace GPL.Commands
 {
+    /// <summary>
+    /// Represents a command interface that defines the Execute method for graphics operations.
+    /// </summary>
     public interface ICommand
     {
+        /// <summary>
+        /// Executes the command with the provided Graphics object.
+        /// </summary>
+        /// <param name="g">The Graphics object on which the command will be executed.</param>
         void Execute(Graphics g);
     }
 }
