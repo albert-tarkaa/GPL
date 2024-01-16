ca7e446ac784ab28aa647986b2c853be04d75620
albert-tarkaa <tarkaa.albert@gmail.com>
2024-01-16 13:08:15 +0100

    feature: Variable Assignment class for variable manipulation

---
+++ GPLv2/Commands/VariableAssignmentCommand.cs
@@ -0,0 +1,153 @@
+﻿using GPL.Utilities;
+using System.Text.RegularExpressions;
+
+namespace GPL.Commands
+{
+    /// <summary>
+    /// This is the Variable Assignment Command class
+    /// </summary>
+    public class VariableAssignmentCommand : ICommand
+    {
+        public string commandItem;
+        readonly DrawingSettings stateManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VariableAssignmentCommand"/> class.
+        /// </summary>
+        /// <param name="cordinatesStateManager">The drawing settings manager.</param>
+        public VariableAssignmentCommand(string commanditem, DrawingSettings cordinatesStateManager)
+        {
+            this.commandItem = commanditem;
+            this.stateManager = cordinatesStateManager;
+        }
+
+        /// <summary>
+        /// Executes the variable assignment command.
+        /// </summary>
+        /// <param name="g">The graphics context for command execution.</param>
+        public void Execute(Graphics g)
+        {
+            ProcessVariableAssignment(commandItem);
+        }
+
+        private void ProcessVariableAssignment(string command)
+        {
+            // Split the command into parts
+            string[] commandParts = command.Split('=');
+            if (commandParts.Length < 2)
+            {
+                throw new InvalidOperationException("Syntax error: Invalid variable assignment");
+            }
+
+            string commandName = commandParts[0].Trim();
+            string commandValue = commandParts[1].Trim();
+
+            // Check if the variable is already declared
+            object variableValue = VariableManager.CheckVariable(commandName);
+
+            if (variableValue != null)
+            {
+                // Check if the initial value is a valid integer
+                if (int.TryParse(commandValue, out int initialValue))
+                {
+                    VariableManager.SetVariable(commandName, initialValue);
+                }
+                else if (IsValidManipulationString(commandValue))
+                {
+                    ProcessVariableManipulation(commandName, commandValue);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Syntax error: Invalid initial value for variable assignment");
+                }
+            }
+            else
+            {
+                // Variable not declared, declare it and assign the value
+                if (int.TryParse(commandValue, out int initialValue))
+                {
+                    VariableManager.SetVariable(commandName, initialValue);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Syntax error: Invalid initial value for variable assignment");
+                }
+            }
+        }
+
+
+        private void ProcessVariableManipulation(string variableName, string manipulationExpression)
+        {
+            // Split the manipulation expression into parts
+            string[] manipulationParts = manipulationExpression.Split(new[] { '+', '-', '*', '/', '%' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (manipulationParts.Length == 2)
+            {
+                // If there are two parts, assume it's a valid manipulation expression
+                string operatorSymbol = manipulationExpression.Replace(manipulationParts[0], "").Replace(manipulationParts[1], "").Trim();
+
+                int value;
+                if (int.TryParse(manipulationParts[1], out value))
+                {
+                    // Perform the manipulation based on the operator
+                    object variableValue = VariableManager.CheckVariable(variableName);
+                    if (variableValue != null)
+                    {
+                        switch (operatorSymbol)
+                        {
+                            case "+":
+                                VariableManager.SetVariable(variableName, (int)variableValue + value);
+                                break;
+                            case "-":
+                                VariableManager.SetVariable(variableName, (int)variableValue - value);
+                                break;
+                            case "*":
+                                VariableManager.SetVariable(variableName, (int)variableValue * value);
+                                break;
+                            case "/":
+                                if (value != 0)
+                                {
+                                    VariableManager.SetVariable(variableName, (int)variableValue / value);
+                                }
+                                else
+                                {
+                                    throw new InvalidOperationException("Division by zero");
+                                }
+                                break;
+                            case "%":
+                                if (value != 0)
+                                {
+                                    VariableManager.SetVariable(variableName, (int)variableValue % value);
+                                }
+                                else
+                                {
+                                    throw new InvalidOperationException("Modulus by zero");
+                                }
+                                break;
+
+                            default:
+                                throw new InvalidOperationException($"Syntax error: Invalid operator in variable manipulation");
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Variable '{variableName}' not declared before manipulation");
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Syntax error: Invalid value in variable manipulation");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"Syntax error: Invalid variable manipulation expression");
+            }
+        }
+        private bool IsValidManipulationString(string value)
+        {
+            return Regex.IsMatch(value, @"^\s*[a-zA-Z][a-zA-Z0-9]*\s*[\+\-\*/%]\s*\d+\s*$");
+        }
+
+    }
+}
