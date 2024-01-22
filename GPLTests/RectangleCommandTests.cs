1845213292ec23ddf7bab63f9d43f6445786b601
albert-tarkaa <tarkaa.albert@gmail.com>
2024-01-22 14:14:37 +0100

    fix: modified Rectangle tests to work with variable assignment and added more tests

--- GPLTests/RectangleCommandTests.cs
+++ GPLTests/RectangleCommandTests.cs
@@ -3,6 +3,7 @@ using GPL.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
+using System.Drawing;
 
 namespace GPLTests
 {
@@ -20,46 +21,81 @@ namespace GPLTests
         {
             // Arrange
             var stateManagerMock = new Mock<DrawingSettings>();
+            string x = "5";
+            string y = "8";
 
             // Act
-            var rectangleCommand = new RectangleCommand(5, 8, stateManagerMock.Object);
+            var rectangleCommand = new RectangleCommand(x, y, stateManagerMock.Object);
 
             // Assert
             Assert.IsNotNull(rectangleCommand);
         }
 
+
         /// <summary>
-        /// Tests the constructor of the <see cref="RectangleCommand"/> class with a negative X parameter.
+        /// Tests the constructor of the <see cref="RectangleCommand"/> class with a negative parameter.
         /// Expects an <see cref="ArgumentOutOfRangeException"/>.
         /// </summary>
         [TestMethod]
-        public void Constructor_WithNegativeX_ThrowsArgumentOutOfRangeException()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_WithNegativeArgs_ThrowsArgumentOutOfRangeException()
         {
             // Arrange
             var stateManagerMock = new Mock<DrawingSettings>();
+            var x = "x";
+            var y = "y";
+            VariableManager.AssignVariable(x, -10);
+            VariableManager.AssignVariable(y, 8);
 
             // Act
-            Action act = () => new RectangleCommand(-5, 8, stateManagerMock.Object);
+            Action act = () => new RectangleCommand(x, y, stateManagerMock.Object);
 
             // Assert
-            Assert.ThrowsException<ArgumentOutOfRangeException>(act, "Negative X should throw ArgumentOutOfRangeException.");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(act, "Value must be greater than 0.");
         }
 
         /// <summary>
-        /// Tests the constructor of the <see cref="RectangleCommand"/> class with a negative Y parameter.
-        /// Expects an <see cref="ArgumentOutOfRangeException"/>.
+        /// Tests the constructor of the <see cref="RectangleCommand"/> class with a null string parameter.
+        /// Expects an <see cref="ArgumentException"/>.
         /// </summary>
         [TestMethod]
-        public void Constructor_WithNegativeY_ThrowsArgumentOutOfRangeException()
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_WithNullArgs_ThrowsArgumentOutOfRangeException()
         {
             // Arrange
             var stateManagerMock = new Mock<DrawingSettings>();
+            var x = "x";
+            var y = "y";
+            VariableManager.AssignVariable(x, "");
+            VariableManager.AssignVariable(y, 8);
 
             // Act
-            Action act = () => new RectangleCommand(5, -8, stateManagerMock.Object);
+            Action act = () => new RectangleCommand(x, y, stateManagerMock.Object);
 
             // Assert
-            Assert.ThrowsException<ArgumentOutOfRangeException>(act, "Negative Y should throw ArgumentOutOfRangeException.");
+            Assert.ThrowsException<ArgumentException>(act, "Value cannot be null or empty.");
+        }
+
+        /// <summary>
+        /// Tests the constructor of the <see cref="RectangleCommand"/> class with a zero parameter.
+        /// Expects an <see cref="ArgumentOutOfRangeException"/>.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Constructor_WithZeroValues_ThrowsArgumentOutOfRangeException()
+        {
+            // Arrange
+            var stateManagerMock = new Mock<DrawingSettings>();
+            var x = "x";
+            var y = "y";
+            VariableManager.AssignVariable(x, 0);
+            VariableManager.AssignVariable(y, 8);
+
+            // Act
+            Action act = () => new RectangleCommand(x, y, stateManagerMock.Object);
+
+            // Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(act, "Value must be greater than zero.");
         }
     }
 }
